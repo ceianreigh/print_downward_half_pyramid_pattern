1d853c93c3873f1c4d52e7354f164803750c05ac
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-22 21:51:20 +0800

    Add the instruction and pseudocode

---
+++ print_downward_half_pyramid_pattern.py
@@ -0,0 +1,11 @@
+# Print a downward Half-Pyramid Pattern of Star (asterisk)
+
+# pseudocode
+
+# using nested for loop
+
+# outer loop for number of rows
+
+# inner loop for number of columns
+
+# print new line after inner loop
